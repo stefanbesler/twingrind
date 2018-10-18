f72c7c340996886a92e9c62094fd87fe4c436d15
Stefan Besler <stefan@besler.me>
2018-10-19 01:01:59 +0200

    added option to not trigger a new measurment (Will read old data)=

--- read_profile.py
+++ read_profile.py
@@ -23,12 +23,15 @@ stores the corresponding callstack on disk""")
 parser.add_argument("-n", "--netid", help="netid of the target machine", required=False)
 parser.add_argument("-p", "--port", help="port of the plc", default=851, required=False)
 parser.add_argument("-d", "--dest", help="output directory ", default="./", required=False)
+parser.add_argument("-n", "--notrigger", help="if set, the script won't trigger, but simply"\
+                    "read the last available data from the plc", action="store_true")
 args = vars(parser.parse_args())
 
 logging.basicConfig(level=logging.DEBUG)
 netid = args['netid']
 port = args['port']
 dest = args['dest']
+trigger = not args['notrigger']
 
 try:
     logging.debug('connecting {}:{}'.format(netid, port))
@@ -36,8 +39,9 @@ try:
     plc = pyads.Connection(netid, port)
     plc.open()
 
-    logging.debug('trigger measurement')
-    plc.write_by_name('Global.profiler.disabled', False, pyads.bool)
+    if trigger:
+        logging.debug('trigger measurement')
+        plc.write_by_name('Global.profiler.disabled', False, pyads.bool)
 
     # wait (we dont really have to since 1 ads call should take at least
     # 1 cycle of the plc anyway, but lets be sure ...
@@ -49,7 +53,7 @@ try:
     # define a new stack class that can contain as many calls as were
     # actually performed - yes, we can do that with python :)
     class Stack(ctypes.Structure):
-        _fields_ = [("calls", ctypes.c_float * nstack)]
+        _fields_ = [("calls", Call * nstack)]
 
     stack = plc.read_by_name('Global.profiler.stack', Stack)
 
