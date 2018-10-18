02757338c9a665d3d34141932dbfe8da6939aeb2
Stefan Besler <stefan@besler.me>
2018-10-19 00:45:06 +0200

    initial commit of the triggering script, blind coded on a Linux machine without access to a twincat installation, don#t point fingers :P

---
+++ read_profile.py
@@ -0,0 +1,64 @@
+import os
+import re
+import logging
+import time
+import datetime
+from argparse import ArgumentParser
+import pyads
+import ctypes
+import pickle
+
+class Call(ctypes.Structure):
+    _fields_ = [("fb_name", ctypes.c_byte * 81),
+                ("method_name", ctypes.c_byte * 81),
+                ("depth", ctypes.c_int),
+                ("startlo", ctypes.c_int),
+                ("starthi", ctypes.c_int),
+                ("endlo", ctypes.c_int),
+                ("endhi", ctypes.c_int)]
+
+parser = ArgumentParser("""triggers the measurement of a single profile and
+stores the corresponding callstack on disk""")
+
+parser.add_argument("-n", "--netid", help="netid of the target machine", required=False)
+parser.add_argument("-p", "--port", help="port of the plc", default=851, required=False)
+parser.add_argument("-d", "--dest", help="output directory ", default="./", required=False)
+args = vars(parser.parse_args())
+
+logging.basicConfig(level=logging.DEBUG)
+netid = args['netid']
+port = args['port']
+dest = args['dest']
+
+try:
+    logging.debug('connecting {}:{}'.format(netid, port))
+
+    plc = pyads.Connection(netid, port)
+    plc.open()
+
+    logging.debug('trigger measurement')
+    plc.write_by_name('Global.profiler.disabled', False, pyads.bool)
+
+    # wait (we dont really have to since 1 ads call should take at least
+    # 1 cycle of the plc anyway, but lets be sure ...
+    time.sleep(1)
+
+    logging.debug('reading measurement')
+    nstack = plc.read_by_name('Global.profiler.nstack', False, pyads.dint)
+
+    # define a new stack class that can contain as many calls as were
+    # actually performed - yes, we can do that with python :)
+    class Stack(ctypes.Structure):
+        _fields_ = [("calls", ctypes.c_float * nstack)]
+
+    stack = plc.read_by_name('Global.profiler.stack', False, Stack)
+
+    logging.debug('store measurement')
+    pickle.dump(stack, open(os.path.join(dest, "callstack_{}".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))), "wb"))
+
+except pyads.ADSError as e:
+    logging.error(e)
+finally:
+    plc.close()
+
+
