cee344ae21c8057a6595db0527bfed1ff6fe9782
Stefan Besler <stefan@besler.me>
2018-10-19 00:03:27 +0200

    formatting and refactoring

--- profiler_guards.py
+++ profiler_guards.py
@@ -18,6 +18,7 @@ filepath = args['directory']
 action = args['action']
 tag = r"(* @@ PROFILER @@ *)"
 
+
 def find_files(filepath):
     """walk recursively through folders and look for TwinCat3 source files"""
 
@@ -28,57 +29,51 @@ def find_files(filepath):
                 yield os.path.join(subdir, f)
 
 
-def add_guards(filepath):
+def add_guards(filepath, fb):
     """add guards to fb and all methods for this file"""
     global tag
-
-    filename, _ = os.path.splitext(os.path.basename(filepath))
 
     with open(filepath, "rt") as f:
         src = f.read()
         if tag in src:
-            logging.warning("profiler guards seem already present in {}, skipping".format(os.path.basename(f)))
+            logging.warning("profiler guards seem already present in {}, skipping".format(fb))
             return
 
         src_new,i = re.subn(r'<Method(.*?)Name="(.*?)"(.*?)<ST><!\[CDATA\[(.*?)\]\]><\/ST>',
-                            r'<Method\1Name="\2"\3<ST><![CDATA[{tag}Profiler.pushMethod("{fb}", "\2"); {tag}\n\4\n{tag}Profiler.popMethod("{fb}", "\2"); {tag}]]></ST>'.format(fb=filename, tag=tag), src, 0, re.S | re.M | re.UNICODE)
+                            r'<Method\1Name="\2"\3<ST><![CDATA[{tag}Profiler.pushMethod("{fb}", "\2");' \
+                            r'{tag}\n\4\n{tag}Profiler.popMethod("{fb}", "\2"); {tag}]]></ST>'.format(fb=fb, tag=tag),
+                            src, 0, re.S | re.M | re.UNICODE)
 
-        if i == 0:
-            logging.information("no profile guards added!")
-        else:
-            logging.debug("{}: guards added in {} methods".format(filename, i))
-
+        logging.debug("{}: guards added in {} methods".format(fb, i))
 
 
     with open(filepath, "wt") as g:
         g.write(src_new)
 
 
-def remove_guards(filepath):
+def remove_guards(filepath, fb):
     """remove guards to fb and all methods for this file"""
     global tag
-
-    filename, _ = os.path.splitext(os.path.basename(filepath))
 
     with open(filepath, "rt") as f:
         src = f.read()
         src_new,i = re.subn(r'[\s\n]*{tag}.*?{tag}[\s\n]*'.format(tag=re.escape(tag)), '', src, 0, re.S | re.M | re.UNICODE)
 
-        if i == 0:
-            logging.information("no profile guards removed!")
-        else:
-            logging.debug("{}: guards removed in {} methods".format(filename, int(i/2))) # we should have 2 guards per method
-
+        logging.debug("{}: guards removed in {} methods".format(fb, int(i/2))) # we should have 2 guards per method
 
 
     with open(filepath, "wt") as g:
         g.write(src_new)
 
 
-for f in find_files(filepath):
-    if action == "add": 
-        add_guards(f)
-    elif action == "remove":
-        remove_guards(f)
-    else:
-        raise Exception('invalid action {}'.format(action))
+# main
+if __name__ == '__main__':
+    for f in find_files(filepath):
+        fb, _ = os.path.splitext(os.path.basename(f))
+
+        if action == "add":
+            add_guards(f, fb)
+        elif action == "remove":
+            remove_guards(f, fb)
+        else:
+            raise Exception('invalid action {}'.format(action))
