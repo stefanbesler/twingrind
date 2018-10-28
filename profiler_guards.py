1b2fc4932717f6ff0c3286010989871ad8708b3f
Stefan Besler <stefanbesler@users.noreply.github.com>
2018-10-28 19:37:04 +0100

    Update profiler_guards.py

--- profiler_guards.py
+++ profiler_guards.py
@@ -4,11 +4,15 @@
 import os
 import re
 import logging
+import pickle
+import copy
+import datetime
 from argparse import ArgumentParser
 
 parser = ArgumentParser("""adds or removes guards to TwinCAT3 function blocks.
 These guards are used for profiling your program""")
 parser.add_argument("-d", "--directory", help="directory containing all function blocks that profiling guards should be modified", required=True)
+parser.add_argument("-o", "--hash_directory", help="directory where the generated hashmap is stored (if action=add)", required=True)
 parser.add_argument("-a", "--action", help="whether guards should be added or removed", choices=["add", "remove"], required=True)
 args = vars(parser.parse_args())
 
@@ -16,6 +20,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 filepath = args['directory']
 action = args['action']
+dest = args['hash_directory']
 tag = r"(* @@ PROFILER @@ *)"
 
 
@@ -29,35 +34,82 @@ def find_files(filepath):
                 yield os.path.join(subdir, f)
 
 
-def add_guards(filepath, fb):
+def create_hash(fb, method, hashes):
+
+    increment = 0
+
+    # todo find a better way to create a hash, if we ever exceed the 65000 we get into trouble
+    while True:
+        hstr = fb + "::" + method + str(increment)
+        h = hash(hstr) & 65535
+        if h not in hashes:
+            hashes[h] = (fb, method)
+            return h
+        increment += 1
+
+
+def add_guards(filepath, fb_name, hashes):
     """add guards to fb and all methods for this file"""
 
     with open(filepath, "rt") as f:
         src = f.read()
         if tag in src:
-            logging.warning("profiler guards seem already present in {}, skipping".format(fb))
+            logging.warning("profiler guards seem already present in {}, skipping".format(fb_name))
             return
 
-        src_new,i = re.subn(r'<Method(.*?)Name="(.*?)"(.*?)<ST><!\[CDATA\[(.*?)\]\]><\/ST>',
-                            r'<Method\1Name="\2"\3<ST><![CDATA[{tag}Profiler.pushMethod("{fb}", "\2");' \
-                            r'{tag}\n\4\n{tag}Profiler.popMethod("{fb}", "\2"); {tag}]]></ST>'.format(fb=fb, tag=tag),
-                            src, 0, re.S | re.M | re.UNICODE)
+    methods = re.findall(r'<Method(.*?)Name="(.*?)"(.*?)<ST><!\[CDATA\[(.*?)\]\]><\/ST>', src, re.S | re.M | re.UNICODE)
+    nearly = 0
+    nmethods = 0
+    if methods:
+        for m in methods:
+            method_name = m[1]
+            body = m[3]
+            old_body = copy.deepcopy(body)
+            hash = create_hash(fb_name, method_name, hashes)
 
-        logging.debug("{}: guards added in {} methods".format(fb, i))
+            body = '''{tag}Global.profiler.pushMethod({hash});{tag}\n'''.format(hash=hash, tag=tag) + body
+            body, i = re.subn(r'RETURN([\s]*?);',
+                                r'''\1{tag}Global.profiler.popMethod({hash}); {tag}\1RETURN;'''.format(hash=hash, tag=tag),
+                                body, 0, re.S | re.M | re.UNICODE)
+            body = body + '''\n{tag}Global.profiler.popMethod({hash});{tag}'''.format(hash=hash, tag=tag)
+
+            nearly += i # two guards are always added
+            nmethods += 1
+
+            s = r'<Method{spacer0}Name="{method_name}"{spacer2}<ST><!\[CDATA\[{body}\]\]><\/ST>'.format(spacer0=m[0],
+                                                                                                        method_name=method_name,
+                                                                                                        spacer2=m[2],
+                                                                                                        body=old_body,
+                                                                                                        fb=fb_name)
+
+            src = src.replace(r'<Method{spacer0}Name="{method_name}"{spacer2}<ST><![CDATA[{body}]]></ST>'.format(spacer0=m[0],
+                                                                                          method_name=method_name,
+                                                                                          spacer2=m[2],
+                                                                                          body=old_body,
+                                                                                          fb=fb_name),
+                              r'<Method{spacer0}Name="{method_name}"{spacer2}<ST><![CDATA[{body}]]></ST>'.format(spacer0=m[0],
+                                                                                          method_name=method_name,
+                                                                                          spacer2=m[2],
+                                                                                          body=body,
+                                                                                          fb=fb_name))
 
 
+
+    logging.debug("{}: guards added in {} methods - {} early RETURNS".format(fb_name, nmethods, nearly))
+
+    #print(src)
     with open(filepath, "wt") as g:
-        g.write(src_new)
+        g.write(src)
 
 
-def remove_guards(filepath, fb):
+def remove_guards(filepath, fb_name):
     """remove guards to fb and all methods for this file"""
 
     with open(filepath, "rt") as f:
         src = f.read()
-        src_new,i = re.subn(r'[\s\n]*{tag}.*?{tag}[\s\n]*'.format(tag=re.escape(tag)), '', src, 0, re.S | re.M | re.UNICODE)
+        src_new, i = re.subn(r'([\s\n]*){tag}.*?{tag}[\s\n]*'.format(tag=re.escape(tag)), '\1', src, 0, re.S | re.M | re.UNICODE)
 
-        logging.debug("{}: guards removed in {} methods".format(fb, int(i/2))) # we should have 2 guards per method
+        logging.debug("{}: guards removed in {} methods".format(fb_name, int(i/2))) # we should have 2 guards per method
 
 
     with open(filepath, "wt") as g:
@@ -66,12 +118,22 @@ def remove_guards(filepath, fb):
 
 # main
 if __name__ == '__main__':
+    hashes = {}
+    main_hash = 0
+    if action == "add":
+        hashes[main_hash] = ('MAIN', 'MAIN')
+
     for f in find_files(filepath):
-        fb, _ = os.path.splitext(os.path.basename(f))
+        fb_name, _ = os.path.splitext(os.path.basename(f))
 
         if action == "add":
-            add_guards(f, fb)
+            add_guards(f, fb_name, hashes)
         elif action == "remove":
-            remove_guards(f, fb)
+            remove_guards(f, fb_name)
         else:
             raise Exception('invalid action {}'.format(action))
+
+    if action == "add":
+        pickle.dump(hashes, open(os.path.join(dest, "hashmap_{}".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))), "wb"))
+
+        print('use {} as hash in your MAIN'.format(main_hash))
