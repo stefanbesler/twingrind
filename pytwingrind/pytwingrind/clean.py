8dbb683f8926dbe49cb973234969dbcd72b8103e
stefanbesler <11271989+stefanbesler@users.noreply.github.com>
2022-04-14 13:54:18 +0200

    feat: detect encoding before writing / reading a file

--- pytwingrind/pytwingrind/clean.py
+++ pytwingrind/pytwingrind/clean.py
@@ -9,14 +9,15 @@ from pytwingrind import common
 def remove_guards(filepath: string, fb_name: string):
     """remove guards to fb and all methods for this file"""
 
-    with open(filepath, "rt") as f:
+    encoding = common.detect_encoding(filepath)
+    with open(filepath, "rt", encoding=encoding) as f:
         src = f.read()
         src_new, i = re.subn(r'([\s\n]*){tag}.*?{tag}[\s\n]*'.format(tag=re.escape(common.profiler_tag)), '\1', src, 0, re.S | re.M | re.UNICODE)
 
         logging.debug("{}: guards removed in {} methods".format(fb_name, int(i/2))) # we should have 2 guards per method
 
 
-    with open(filepath, "wt") as g:
+    with open(filepath, "wt", encoding=encoding) as g:
         g.write(src_new)
 
 def run(dest: str):
