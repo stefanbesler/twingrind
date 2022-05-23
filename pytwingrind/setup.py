78909f98492fb62a24a10f4b2482e4875b7ec784
Stefan Besler <stefan@besler.me>
2022-05-23 17:02:25 +0200

    chore: bump version to 0.2.1

--- pytwingrind/setup.py
+++ pytwingrind/setup.py
@@ -5,7 +5,7 @@ from os import environ
 
 setup(
     name="pytwingrind",
-    version=f"0.2.0",
+    version=f"0.2.1",
     author="Stefan Besler",
     author_email="stefan@besler.me",
     description="Call-graph profiling for TwinCAT 3.",
